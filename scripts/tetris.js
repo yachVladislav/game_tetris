8d8204438542c74125cf874053eb91143ab1f51c
yach.vladislav <v.yachmeniov@complitech.org>
2024-01-27 13:47:43 +0300

    move figure

--- scripts/tetris.js
+++ scripts/tetris.js
@@ -34,4 +34,64 @@ export default class Tatris{
             [0,0,0]
         ]
     };
+
+    moveLeft() {
+        this.activeFigure.x -= 1;
+
+        if(this.hasCollision()){
+            this.activeFigure.x += 1;
+        }
+    }
+
+    moveRight() {
+        this.activeFigure.x += 1;
+
+        if(this.hasCollision()){
+            this.activeFigure.x -= 1;
+        }
+    }
+
+    moveDown() {
+        this.activeFigure.y += 1;
+
+        if(this.hasCollision()){
+            this.activeFigure.y -= 1;
+            this.lockFigure();
+        }
+    }
+
+    hasCollision() {
+        const {x: figureX, y: figureY, block} = this.activeFigure;
+
+        for(let y=0; block.length; y++) {
+
+            for(let x=0; block[y].length; x++) {
+                
+                if(block[y][x] && 
+                    ((this.field[figureY + y] === undefined || this.field[figureY + y][figureX + x] === undefined) || 
+                    this.field[figureY + y][figureX + x])
+                    ) {
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    lockFigure() {
+        const {x: figureX, y: figureY, block} = this.activeFigure;
+
+        for(let y=0; block.length; y++) {
+
+            for(let x=0; block[y].length; x++) {
+                
+                if(block[y][x]) {
+
+                this.field[figureY + y][figureX + x] = block[y][x];
+                }
+            }
+        }
+    }
 }
